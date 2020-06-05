c22251e15785d418fc393063f8b3c5bcfd58c343
Alberto Araiza <alberto.araiza88@gmail.com>
2020-06-05 18:13:53 -0500

    change to external storage

--- app/src/main/java/test/isavanzados/textadding/MainActivity.kt
+++ app/src/main/java/test/isavanzados/textadding/MainActivity.kt
@@ -1,13 +1,18 @@
 package test.isavanzados.textadding
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.FileOutputStream
 import java.io.FileWriter
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -15,17 +20,47 @@ import java.nio.file.StandardOpenOption
 
 class MainActivity : AppCompatActivity() {
     val FILE_NAME = "data.txt"
+    val REQUEST_CODE = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
 
         btnGuardarTexto.setOnClickListener {
             val value = etTexto.text.toString()
-            val fos = openFileOutput(FILE_NAME, Context.MODE_APPEND)
-            fos.write(value.toByteArray())
+            val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),"/$FILE_NAME")
+            val fileOutputStream = FileOutputStream(file, true)
+            fileOutputStream.write(value.toByteArray())
+            fileOutputStream.close()
             etTexto.text.clear()
-            Toast.makeText(this, "guardado en: ${filesDir}/$FILE_NAME", Toast.LENGTH_LONG ).show()
-            fos?.close()
         }
+    }
+
+  fun checkPermission(permission: String) {
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission),REQUEST_CODE)
+        }
+        else {
+            Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == REQUEST_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+                finish()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
